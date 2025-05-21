fb66317560fea901fb7912ea13d0ae4083365360
JulioPeixoto <juliorafaelnft@hotmail.com>
2025-05-21 17:33:49 -0300

    add softrag_llm file

--- src/examples/simple.py
+++ src/examples/simple.py
@@ -8,6 +8,9 @@ add content from a web page, and perform a query.
 from softrag import Rag 
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 
@@ -15,10 +18,9 @@ chat = ChatOpenAI(model="gpt-4o", api_key=api_key, streaming=True)
 embed = OpenAIEmbeddings(model="text-embedding-3-small", api_key=api_key)
 
 rag = Rag(embed_model=embed, chat_model=chat)
+#rag.add_web(url="https://pt.wikipedia.org/wiki/Python")
+rag.add_file("softrag_llm.md")
 
-rag.add_web(url="https://pt.wikipedia.org/wiki/Python")
-rag.add_file("README.md")
-
-for chunk in rag.query("What are the main features of the Python programming language?", stream=True):
+for chunk in rag.query("how can i integrate softrag with chatgpt?", stream=True):
     print(chunk, end="", flush=True) 
     
